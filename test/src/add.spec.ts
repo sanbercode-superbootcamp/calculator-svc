9dee71e6c3c2f575c31b6861d3120863fde9905d
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-14 12:05:31 +0700

    add blackbox testing when error

--- test/src/add.spec.ts
+++ test/src/add.spec.ts
@@ -1,4 +1,5 @@
 import { get } from 'request-promise-native';
+import { StatusCodeError } from 'request-promise-native/errors';
 import { expect } from 'chai';
 
 describe('calculator server', function(){
@@ -11,6 +12,42 @@ describe('calculator server', function(){
             expect(result).to.be.deep.eq({
                 result: 7
             });
+        });
+
+        it.only('should error when input is not number', async function(){
+            let error = null;
+            try{
+                const result = await get('http://localhost:3000/add?n1=4&n2=hahas', { json: true});
+            } catch(err){
+                error = err;
+            }
+            expect(error).to.be.exist;
+            expect(error).to.be.instanceOf(StatusCodeError);
+            expect(error.statusCode).to.be.eq(400);
+        });
+
+        it.only('should error when no input', async function(){
+            let error = null;
+            try{
+                const result = await get('http://localhost:3000/add', { json: true});
+            } catch(err){
+                error = err;
+            }
+            expect(error).to.be.exist;
+            expect(error).to.be.instanceOf(StatusCodeError);
+            expect(error.statusCode).to.be.eq(400);
+        });
+
+        it.only('should error when no endpoint', async function(){
+            let error = null;
+            try{
+                const result = await get('http://localhost:3000/asdasdsadsad', { json: true});
+            } catch(err){
+                error = err;
+            }
+            expect(error).to.be.exist;
+            expect(error).to.be.instanceOf(StatusCodeError);
+            expect(error.statusCode).to.be.eq(404);
         });
     });
 });
