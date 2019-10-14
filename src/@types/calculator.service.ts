76718044fcb1fa082dd73391310a009a1b1c792d
arifintahu <miftahul97@gmail.com>
2019-10-14 13:40:08 +0700

    add substract multiply division function

--- src/@types/calculator.service.ts
+++ src/@types/calculator.service.ts
@@ -1,13 +1,44 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import { parse } from 'url'
-import { add } from './calculator'
+import { add, substract, multiply, division } from './calculator'
 
 export function addService(req: IncomingMessage, res: ServerResponse){
     //parsing request
     const url = parse(req.url, true)
     const query = url.query
-    console.log('query : ',query)
-    console.log(query['n1'], query['n2'])
+
+    if(!query['n1'] || !query['n2']){
+        res.statusCode = 400
+        res.end('perlu query')
+        return
+    }
+
+    //n1 & n2 harus angka
+    const n1 = parseInt(query['n1'] as string, 10)
+    const n2 = parseInt(query['n2'] as string, 10)
+
+    if(isNaN(n1) || isNaN(n2)){
+        res.statusCode=400
+        res.end('isNaN')
+        return
+    }
+
+    const n3 =  add(n1, n2)
+
+    const output = {
+        result: n3
+    }
+
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(output))
+    res.end()
+}
+
+export function substractService(req: IncomingMessage, res: ServerResponse){
+    //parsing request
+    const url = parse(req.url, true)
+    const query = url.query
+
     if(!query['n1'] || !query['n2']){
         res.statusCode = 400
         res.end('perlu query')
@@ -22,9 +53,68 @@ export function addService(req: IncomingMessage, res: ServerResponse){
         res.end('isNaN')
         return
     }
-    console.log(n1, n2)
 
-    const n3 =  add(n1, n2)
+    const n3 =  substract(n1, n2)
+
+    const output = {
+        result: n3
+    }
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(output))
+    res.end()
+}
+
+export function multiplyService(req: IncomingMessage, res: ServerResponse){
+    //parsing request
+    const url = parse(req.url, true)
+    const query = url.query
+
+    if(!query['n1'] || !query['n2']){
+        res.statusCode = 400
+        res.end('perlu query')
+        return
+    }
+    //n1 & n2 harus angka
+    const n1 = parseInt(query['n1'] as string, 10)
+    const n2 = parseInt(query['n2'] as string, 10)
+
+    if(isNaN(n1) || isNaN(n2)){
+        res.statusCode=400
+        res.end('isNaN')
+        return
+    }
+
+    const n3 =  multiply(n1, n2)
+
+    const output = {
+        result: n3
+    }
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(output))
+    res.end()
+}
+
+export function divisionService(req: IncomingMessage, res: ServerResponse){
+    //parsing request
+    const url = parse(req.url, true)
+    const query = url.query
+
+    if(!query['n1'] || !query['n2']){
+        res.statusCode = 400
+        res.end('perlu query')
+        return
+    }
+    //n1 & n2 harus angka
+    const n1 = parseInt(query['n1'] as string, 10)
+    const n2 = parseInt(query['n2'] as string, 10)
+
+    if(isNaN(n1) || isNaN(n2)){
+        res.statusCode=400
+        res.end('isNaN')
+        return
+    }
+
+    const n3 =  division(n1, n2)
 
     const output = {
         result: n3
--- src/@types/server.ts
+++ src/@types/server.ts
@@ -1,12 +1,21 @@
 import { createServer } from 'http'
 import { parse } from 'url';
-import { addService } from './calculator.service';
+import { addService, substractService, multiplyService, divisionService } from './calculator.service';
 
 const server = createServer((req, res) => {
     const url = parse(req.url)
     switch (url.pathname) {
         case '/add':
             addService(req, res)
+            break;
+        case '/substract':
+            substractService(req, res)
+            break;
+        case '/multiply':
+            multiplyService(req, res)
+            break;
+        case '/division':
+            divisionService(req, res)
             break;
         default:
             res.statusCode = 404
