fe5d35d6792decc04e018ea5f4e661fa25625627
nandyo <nandyo6969@gmail.com>
2019-10-14 14:42:43 +0700

    Bikin calculator servisnya

--- src/calculator.service.ts
+++ src/calculator.service.ts
@@ -1,36 +1,42 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { parse } from 'url';
-import { add } from "./calculator";
+import { ServerResponse, IncomingMessage } from "http";
+import { parse } from "url";
+import { tambah, kurang, kali, bagi } from './calculator';
+export function calculatorService(req: IncomingMessage, res: ServerResponse){
+    //paring request
+    const url = parse(req.url, true);
+    const query = url.query;
+    //if n1 & n2 harus ada
+    if (!query["n1"] || !query["n2"]){
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+    
+    //n1 & n2 harus angka
+    const n1 = parseInt(query["n1"] as string, 10);
+    const n2 = parseInt(query["n2"] as string, 10);
+    if(isNaN(n1) || isNaN(n2)){
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
 
-export function addService(req: IncomingMessage, res: ServerResponse) {
-  // parsing parameters
-  const url = parse(req.url, true);
-  const query = url.query;
-  
-  // n1 & n2 harus ada
-  if (!query['n1'] || !query['n2']) {
-    res.statusCode = 400;
-    res.end();
-    return;
-  }
+    //switch case operator
+    console.log(url.pathname);
+    let n3 = null;
+    switch(url.pathname){
+    case "/tambah": n3 = tambah(n1, n2);
+    break;
+    case "/kurang": n3 = kurang(n1, n2);
+    break;
+    case "/kali": n3 = kali(n1, n2);
+    break;
+    case "/bagi": n3 = bagi(n1, n2);
+    break;
+    }
 
-  // n1 & n2 harus angka
-  const n1 = parseInt(query['n1'] as string, 10);
-  const n2 = parseInt(query['n2'] as string, 10);
-  if(isNaN(n1) || isNaN(n2)){
-    res.statusCode = 400;
-    res.end();
-    return;
-  }
+    //encode reuslt
 
-  // add
-  const n3 = add(n1, n2);
-
-  // encode results
-  const output = {
-    result: n3
-  };
-  res.setHeader('Content-Type', 'application/json');
-  res.write(JSON.stringify(output));
-  res.end();
+    // console.log(url.query);
+    res.end(n3.toString());
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
 import { createServer } from 'http';
-import { parse } from 'url';
-import { addService } from './calculator.service';
+import { parse } from "url";
+import { calculatorService } from "./calculator.service";
 
 const server = createServer((req, res) => {
-  const url = parse(req.url);
-  switch(url.pathname){
-    case '/add':
-      addService(req, res);
-      break;
-    default:
-      res.statusCode = 404;
-      res.end();
-  }
+    const url = parse(req.url);
+    switch(url.pathname){
+        case "/tambah": calculatorService(req, res);
+        break;
+        case "/kurang": calculatorService(req, res);
+        break;
+        case "/kali": calculatorService(req, res);
+        break;
+        case "/bagi": calculatorService(req, res);
+        break;
+        default: res.statusCode = 404; res.end();
+    }
+    // if(url.pathname){
+    //     calculatorService(req, res);
+    // }
+    // else{
+    //     res.statusCode = 404; res.end()
+    // }
 });
 
 server.listen(3000);
