245cea9b106faebdbc55752414fffcb304a0cf3a
unknown <andria.rezaandria.reza74@gmail.com>
2019-10-14 14:03:01 +0700

    menambahkan fungsi min, multi, dan div

--- src/calculator.service.ts
+++ src/calculator.service.ts
@@ -1,6 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { parse } from 'url';
-import { add } from './calculator';
+import { add, minus, multi, div } from './calculator';
 
 export function addService(req: IncomingMessage, res: ServerResponse) {
     // PARSING REQUEST
@@ -28,6 +28,93 @@ export function addService(req: IncomingMessage, res: ServerResponse) {
     const output = {
         result: n3
     }
+    res.setHeader("Content-type", 'json');
+    res.write(JSON.stringify(output));
+    res.end();
+}
+
+export function minusService(req: IncomingMessage, res: ServerResponse) {
+    const url = parse(req.url, true);
+    const query = url.query;
+
+    if(!query['n1'] || !query['n2']) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    let n1 = parseInt(query['n1'] as string, 10);
+    let n2 = parseInt(query['n2'] as string, 10);
+    if(isNaN(n1) || isNaN(n2)) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    const n3 = minus(n1, n2);
+
+    const output = {
+        result: n3
+    }
+
+    res.setHeader("Content-type", 'json');
+    res.write(JSON.stringify(output));
+    res.end();
+}
+
+export function multiService(req: IncomingMessage, res: ServerResponse) {
+    const url = parse(req.url, true);
+    const query = url.query;
+
+    if(!query['n1'] || !query['n2']) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    let n1 = parseInt(query['n1'] as string, 10);
+    let n2 = parseInt(query['n2'] as string, 10);
+    if(isNaN(n1) || isNaN(n2)) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    const n3 = multi(n1, n2);
+
+    const output = {
+        result: n3
+    }
+
+    res.setHeader("Content-type", 'json');
+    res.write(JSON.stringify(output));
+    res.end();
+}
+
+export function divService(req: IncomingMessage, res: ServerResponse) {
+    const url = parse(req.url, true);
+    const query = url.query;
+
+    if(!query['n1'] || !query['n2']) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    let n1 = parseInt(query['n1'] as string, 10);
+    let n2 = parseInt(query['n2'] as string, 10);
+    if(isNaN(n1) || isNaN(n2)) {
+        res.statusCode = 400;
+        res.end();
+        return;
+    }
+
+    const n3 = div(n1, n2);
+
+    const output = {
+        result: n3
+    }
+
     res.setHeader("Content-type", 'json');
     res.write(JSON.stringify(output));
     res.end();
